5f6e8e400a51f1dd1676823bffd8491b90c764dc
Minizbot2012 <bradleyfwin@gmail.com>
2023-03-09 21:54:44 +0000

    check the highest form id

--- SynPatcher/Program.cs
+++ SynPatcher/Program.cs
@@ -20,7 +20,8 @@ namespace SynCOBJ
         public static void RunPatch(IPatcherState<ISkyrimMod, ISkyrimModGetter> state)
         {
             var overrides = state.PatchMod.EnumerateMajorRecords().Where(x=>x.FormKey.ModKey != state.PatchMod.ModKey).Count();
-            if (state.PatchMod.ModHeader.Stats.NumRecords - overrides < 4096)
+            var fidcheck = (state.PatchMod.EnumerateMajorRecords().Where(x=>x.FormKey.ModKey == state.PatchMod.ModKey).Select(x=>x.FormKey.ID).Max() <= 0x000FFFFF);
+            if (state.PatchMod.ModHeader.Stats.NumRecords - overrides < 4096 && fidcheck)
             {
                 Console.WriteLine($"ESLIfing patch stack with {overrides} overrides and {state.PatchMod.ModHeader.Stats.NumRecords - overrides} new records");
                 state.PatchMod.ModHeader.Flags |= SkyrimModHeader.HeaderFlag.LightMaster;
