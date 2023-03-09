b89d0d5a9e79f03e5c5c334cd1ce5a8663f6d832
Minizbot2012 <bradleyfwin@gmail.com>
2023-03-09 21:17:20 +0000

    Initial commit

---
+++ SynPatcher/Program.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Threading.Tasks;
+
+using Mutagen.Bethesda;
+using Mutagen.Bethesda.Skyrim;
+using Mutagen.Bethesda.Synthesis;
+
+namespace SynCOBJ
+{
+    internal class Program
+    {
+        public static async Task<int> Main(string[] args)
+        {
+            return await SynthesisPipeline.Instance
+                .AddPatch<ISkyrimMod, ISkyrimModGetter>(RunPatch)
+                .SetTypicalOpen(GameRelease.SkyrimSE, "SynCOBJ.esp")
+                .Run(args);
+        }
+        public static void RunPatch(IPatcherState<ISkyrimMod, ISkyrimModGetter> state)
+        {
+            if(state.PatchMod.ModHeader.Stats.NumRecords < 2048) {
+                Console.WriteLine($"ESLIfing patch stack");
+                state.PatchMod.ModHeader.Flags |= SkyrimModHeader.HeaderFlag.LightMaster;
+            }
+        }
+    }
+}
