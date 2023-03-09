cb9d47b0ec01aaf18fd34cfc3871121abf82868f
Minizbot2012 <bradleyfwin@gmail.com>
2023-03-09 21:20:02 +0000

    Format

--- SynPatcher/Program.cs
+++ SynPatcher/Program.cs
@@ -18,7 +18,8 @@ namespace SynCOBJ
         }
         public static void RunPatch(IPatcherState<ISkyrimMod, ISkyrimModGetter> state)
         {
-            if(state.PatchMod.ModHeader.Stats.NumRecords < 4096) {
+            if (state.PatchMod.ModHeader.Stats.NumRecords < 4096)
+            {
                 Console.WriteLine($"ESLIfing patch stack");
                 state.PatchMod.ModHeader.Flags |= SkyrimModHeader.HeaderFlag.LightMaster;
             }
